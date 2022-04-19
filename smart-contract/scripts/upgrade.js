86cc1b844ed5a3cd07129381b25c051307960f00
bibhash1996 <bibhash1996@gmail.com>
2022-04-19 17:10:35 +0530

    upgrade smart contract script

---
+++ smart-contract/scripts/upgrade.js
@@ -0,0 +1,10 @@
+const { ethers, upgrades } = require('hardhat');
+
+async function main () {
+  const BoxV2 = await ethers.getContractFactory('Box');
+  console.log('Upgrading Box...');
+  await upgrades.upgradeProxy('0x55b44F845BED28f34196d24F58aC095e5A933355', BoxV2);
+  console.log('Box upgraded');
+}
+
+main();
